367b76d2862a309429a568b5edcb6bf28acd5a08
imat <raymart.marasigan@gmail.com>
2015-08-27 13:37:06 +0800

    delete reference module

--- sites/all/modules/custom_certificate/careertastic_agent/email_template/agent_coupon_id.tpl.php
+++
@@ -1,8 +0,0 @@
-		<div class="section-2">
-			<p>
-				Hi <?php echo $user->field_first_name['und'][0]['value']?>,<br/><br/>
-
-				Thanks for joining Careertastic, please use <b><?php echo $coupon->commerce_coupon_code['und'][0]['value']?></b>
-				as your promotional code to get commision on our products.
-		    </p>
-		</div>
